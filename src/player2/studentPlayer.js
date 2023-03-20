8caf97b3124dd988ebec40abbe8f0d78b6f315ab
ritapravo <ritopravosarker@gmail.com>
2023-03-21 01:02:56 +0530

    changed

--- src/player2/studentPlayer.js
+++ src/player2/studentPlayer.js
@@ -440,7 +440,7 @@ const StudentVideoPlayer = () => {
                             <Paper
 
                                 onClick={() => {
-                                    playerRef.current.seekTo(bookmark.time);
+                                    playerRef.current.seekTo(bookmark.time-0.5);
                                 }}
                                 elevation={1}
                             >
